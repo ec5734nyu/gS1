8ab1ee9c2b37d6e6f41a93f7ee0c489a42f4b3e5
ec5734nyu <ec5734@nyu.edu>
2026-01-31 16:47:22 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
     debug_question = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?", 
                       "Are encoding and encryption the same? - Yes/No",
                       "Is it possible to decrypt a message without a key? - Yes/No",
+                      "Is it possible to decode a message without a key? - Yes/No",
                       "Is a hashed message supposed to be un-hashed? - Yes/No",
                       "What is the SHA256 hashing value of your NYU email and use the answer in your code - ",
                       "Is MD5 a secured hashing algorithm? - Yes/No",
